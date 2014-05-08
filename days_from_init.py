7213fa03803f7a79b7ece0d8d0c017926ba42ced
WangYinfeng <wang.batman@gmail.com>
2014-05-08 10:42:10 +0800

    literal the output

--- days_from_init.py
+++ days_from_init.py
@@ -3,4 +3,6 @@ from datetime import datetime
 init_year=2014
 init_month=2
 init_day=6
-print (datetime.now().date()-date(init_year, init_month, init_day)).days
+#print (datetime.now().date()-date(init_year, init_month, init_day)).days 
+days=(datetime.now().date()-date(init_year, init_month, init_day)).days 
+print "Angel has came for %d days." %days
