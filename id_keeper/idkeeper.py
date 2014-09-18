f282b626965e15a3eb956b554d2fec83ec65c2bc
Wang Yinfeng <wang.batman@gmail.com>
2014-09-18 15:07:02 -0400

    requests

---
+++ id_keeper/idkeeper.py
@@ -0,0 +1,29 @@
+# The task is simple, print the password and username for the input key word.
+# First step run as:
+#   #python idkeeper.py gmail
+#   #wang@gmail.com:123456
+#
+# Then next step may provide interactive mode:
+#   #python idkeeper.py
+#   #Input the web name:
+#    > gmail
+#   #Your account for gmail is:
+#   #wang@gmail.com:123456
+#
+# Then next step may provide fuzzy matching in case you can't remember the full/correct name of the web:
+#   #python idkeeper.py
+#   #Input the web name:
+#     > mail
+#   #Do you mean:
+#    gmail
+#    amail
+#    bmail
+#   Select the correct name and input again:
+#    >gmail
+#   #Your account for gmail is:
+#   #wang@gmail.com:123456
+#
+# Then next step may provide binary file to run directly, for both linux and windows.
+
+
+
