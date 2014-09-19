7d2af841904894aec132075e03f80a49e7836a01
WangYinfeng <wang.batman@gmail.com>
2014-09-19 14:36:38 +0800

    finish password encrypt

--- id_keeper/idkeeper.py
+++ id_keeper/idkeeper.py
@@ -23,16 +23,52 @@
 #   #Your account for gmail is:
 #   #wang@gmail.com:123456
 #
-# Then next step should provide encrypt for the accounts content, some accounts are critical.
+# Then next step should provide encrypt for the accounts content, some accounts are critical. DONE
+#
+# Then next step should move the critical data to another file, and encrypt that file with a different way.
 #
 # Then next step may provide binary file to run directly, for both linux and windows.
 from sys import argv
+from code import decode
 
-box = { "pip.io":"mfrc531:16", 
-        "pip":"m:16",
-        "pipop":"f:16",
-        "pinterest":"mfrc531:16", 
-        "kanbox":"batman.wang@qq.com:16"}
+box = {
+        "wifi":"aacZEknf9G7kKGBbolNjaEBI9dHI4X2dlE9TgtepYnc=",
+        "126mail":"4/Z0jtUFUsk2vUt4yks/Xi1pYnLpDPLW7Vnym1J+x1Q=",
+        "douban":"5fHvJfeWPc+g0JS+unEq0Ey/ls5ycdiNWdwrb7nYEpg=",
+        "dangdang":"5fHvJfeWPc+g0JS+unEq0Ey/ls5ycdiNWdwrb7nYEpg=",
+        "amazon":"5fHvJfeWPc+g0JS+unEq0HBcQPu8D1F2e2fMRVJwoU8=",
+        "jd":"2bsS5howcenNxeVNkJQye0BI9dHI4X2dlE9TgtepYnc=",
+        "suning":"2cDxfRliFDLflROCyDCCS9Cgwsh/sDxFEHsiSuDDp8s=",
+        "facebook":"5fHvJfeWPc+g0JS+unEq0GH/1PrT0gVzcAYFrEst84o=",
+        "twitter":"aacZEknf9G7kKGBbolNjaEBI9dHI4X2dlE9TgtepYnc=",
+        "pinterest":"aacZEknf9G7kKGBbolNjaEBI9dHI4X2dlE9TgtepYnc=", 
+        "kanbox":"5fHvJfeWPc+g0JS+unEq0Ey/ls5ycdiNWdwrb7nYEpg=",
+        "dropbox":"5fHvJfeWPc+g0JS+unEq0Ey/ls5ycdiNWdwrb7nYEpg=",
+        "115":"5fHvJfeWPc+g0JS+unEq0Ey/ls5ycdiNWdwrb7nYEpg=",
+        "box":"5fHvJfeWPc+g0JS+unEq0Ey/ls5ycdiNWdwrb7nYEpg=",
+        "jinshan box":"5fHvJfeWPc+g0JS+unEq0Ey/ls5ycdiNWdwrb7nYEpg=",
+        "58":"Xcwy2fogGcZSOveDeK2rd0BI9dHI4X2dlE9TgtepYnc=",
+        "skype":"CgDPTnLwaPj25hhIrzo39ObVNCgs4+uxLBVxmORcDis=",
+        "battlecn":"TQVefQF00lHYsnDFJD+tEkBI9dHI4X2dlE9TgtepYnc=",
+        "csdn":"3pGNOhaH3CAv2cFoFva/NkBI9dHI4X2dlE9TgtepYnc=",
+        "luexiao":"5fHvJfeWPc+g0JS+unEq0Ey/ls5ycdiNWdwrb7nYEpg=",
+        "linkedin":"5fHvJfeWPc+g0JS+unEq0Ey/ls5ycdiNWdwrb7nYEpg=",
+        "baidu":"aacZEknf9G7kKGBbolNjaEBI9dHI4X2dlE9TgtepYnc=",
+        "sanbeidanchi":"5fHvJfeWPc+g0JS+unEq0Ey/ls5ycdiNWdwrb7nYEpg=",
+        "yihaodian":"5fHvJfeWPc+g0JS+unEq0EJbFZJYumZ63yUtAX5v1VU=",
+        "shuishouji":"5fHvJfeWPc+g0JS+unEq0Ey/ls5ycdiNWdwrb7nYEpg=",
+        "evernote":"5fHvJfeWPc+g0JS+unEq0HBcQPu8D1F2e2fMRVJwoU8=",
+        "51job":"WtnuafI18sVeKwQYnehzAEBI9dHI4X2dlE9TgtepYnc=",
+        "windnle":"5fHvJfeWPc+g0JS+unEq0Ey/ls5ycdiNWdwrb7nYEpg=",
+        "8europ":"aacZEknf9G7kKGBbolNjaEBI9dHI4X2dlE9TgtepYnc=",
+        "dingxiangyuan":"AmjQfq6AwJsWEOWDWsrP30BI9dHI4X2dlE9TgtepYnc=",
+        "topcoder":"D3oWBKoUJUzFadk7MQsUzPPGzu2o3Mwqa1u6m+PMiqw=",
+        "pbccrc":"YuKyl1P8pAPqkWQ6JohLQkBI9dHI4X2dlE9TgtepYnc=",
+        "github":"UFYPpswX6ehwiS3r41WzqEBI9dHI4X2dlE9TgtepYnc=",
+        "slideshare":"VawSGufaDgVg1H8k1ZV7zkBI9dHI4X2dlE9TgtepYnc=",
+        "gmail old":"KVn59Plvd9MoQ8hjCJyVu4HKMQ6A89FTj48jT/6QrOo=",
+        "live":"jlls3y89Vpc44E8aPaWVlEqgiNM37TlN93nLnYJGw53QeNK68tGEV707TsOsfpH5QlsVkli6ZnrfJS0Bfm/VVQ=="
+        }
 
 if len(argv) == 2:
     script_name, account = argv
@@ -43,8 +79,8 @@ candidate = []
 if account == 'all':
     for name in box.keys():
         print name,
-        print "\t",
-        print box[name]
+        print "\t\t",
+        print decode(box[name])
 elif box.has_key(account) == False:
     for name in box.keys():
         if name.find(account) == -1:
@@ -54,13 +90,13 @@ elif box.has_key(account) == False:
     if len(candidate):
         print "Do you mean:"
         for n in candidate:
-            print "\t",
+            print "\t\t",
             print n
         account = raw_input(">")    
-        print box[account]
+        print decode(box[account])
     else:
         print "Can't find the account '%s', try the correct name." % account
         exit(1)
 else:
-    print box[account]
+    print decode(box[account])
 
