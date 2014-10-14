0a55bf0a5d0fe9790e75b82c90dcde2512f42cb5
Wang Yinfeng <wang.batman@gmail.com>
2014-10-14 09:38:17 -0400

    request to calucate days to specified date

--- days_from_init.py
+++ days_from_init.py
@@ -1,3 +1,5 @@
+#TODO: provide ability to calculate days between init day and specified day.
+#      if no specified day(parameter) is provided, specified day is today.
 from datetime import date
 from datetime import datetime
 init_year=2014
@@ -6,3 +8,4 @@ init_day=6
 #print (datetime.now().date()-date(init_year, init_month, init_day)).days 
 days=(datetime.now().date()-date(init_year, init_month, init_day)).days 
 print "Angel has came for %d days." %days
+
