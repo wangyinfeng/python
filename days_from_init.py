eba0c3840f6ca0dd9612f59fd547a5ca5097bddf
Wang Yinfeng <wang.batman@gmail.com>
2014-10-15 11:15:39 -0400

    provide query from specified day to the init day

--- days_from_init.py
+++ days_from_init.py
@@ -1,11 +1,25 @@
-#TODO: provide ability to calculate days between init day and specified day.
-#      if no specified day(parameter) is provided, specified day is today.
+from sys import argv
 from datetime import date
 from datetime import datetime
+
 init_year=2014
 init_month=2
 init_day=6
+
+# the input argv has format like 2014.10.15, split year.month.day with '.'
+if len(argv) == 2:
+#    script_name, date = argv # TypeError: 'str' object is not callable. date is a build-in name
+    script_name, the_date = argv
+    # so damn easy to split the string.
+    specified_date=the_date.split(".") 
+    the_year=int(specified_date[0])
+    the_month=int(specified_date[1])
+    the_day=int(specified_date[2])
+
+    days=(date(the_year, the_month, the_day)-date(init_year, init_month, init_day)).days 
+    print "To %s, Angel has came for %d days." %(the_date, days)
+else:
 #print (datetime.now().date()-date(init_year, init_month, init_day)).days 
-days=(datetime.now().date()-date(init_year, init_month, init_day)).days 
-print "Angel has came for %d days." %days
+    days=(datetime.now().date()-date(init_year, init_month, init_day)).days 
+    print "Angel has came for %d days." %days
 
