4f3c8843e2e541d65b2ec08d38372bbfcad859c3
Wang Yinfeng <wang.batman@gmail.com>
2014-12-02 17:46:31 -0500

    seprate data from code

--- id_keeper/idkeeper.py
+++ id_keeper/idkeeper.py
@@ -24,66 +24,31 @@
 #   #wang@gmail.com:123456
 #
 # Then next step should provide encrypt for the accounts content, some accounts are critical. DONE
-#
+# 
 # Then next step should move the critical data to another file, and encrypt that file with a different way.
+#
+# I hope when want to add something new, only need to do one operation, input the account and the password, then the coder encrypt the password and insert the accound info to the hash table automatially. Anyway, the encrypt result is not readable and show it out is meanless.
 #
 # Then next step may provide binary file to run directly, for both linux and windows.
 from sys import argv
 from code import decode
 
-box = {
-        "wifi":"aacZEknf9G7kKGBbolNjaEBI9dHI4X2dlE9TgtepYnc=",
-        "126mail":"4/Z0jtUFUsk2vUt4yks/Xi1pYnLpDPLW7Vnym1J+x1Q=",
-        "douban":"5fHvJfeWPc+g0JS+unEq0Ey/ls5ycdiNWdwrb7nYEpg=",
-        "dangdang":"5fHvJfeWPc+g0JS+unEq0Ey/ls5ycdiNWdwrb7nYEpg=",
-        "amazon":"5fHvJfeWPc+g0JS+unEq0HBcQPu8D1F2e2fMRVJwoU8=",
-        "jd":"2bsS5howcenNxeVNkJQye0BI9dHI4X2dlE9TgtepYnc=",
-        "suning":"2cDxfRliFDLflROCyDCCS9Cgwsh/sDxFEHsiSuDDp8s=",
-        "facebook":"5fHvJfeWPc+g0JS+unEq0GH/1PrT0gVzcAYFrEst84o=",
-        "twitter":"aacZEknf9G7kKGBbolNjaEBI9dHI4X2dlE9TgtepYnc=",
-        "pinterest":"aacZEknf9G7kKGBbolNjaEBI9dHI4X2dlE9TgtepYnc=", 
-        "kanbox":"5fHvJfeWPc+g0JS+unEq0Ey/ls5ycdiNWdwrb7nYEpg=",
-        "dropbox":"5fHvJfeWPc+g0JS+unEq0Ey/ls5ycdiNWdwrb7nYEpg=",
-        "115":"5fHvJfeWPc+g0JS+unEq0Ey/ls5ycdiNWdwrb7nYEpg=",
-        "box":"5fHvJfeWPc+g0JS+unEq0Ey/ls5ycdiNWdwrb7nYEpg=",
-        "jinshan box":"5fHvJfeWPc+g0JS+unEq0Ey/ls5ycdiNWdwrb7nYEpg=",
-        "58":"Xcwy2fogGcZSOveDeK2rd0BI9dHI4X2dlE9TgtepYnc=",
-        "skype":"CgDPTnLwaPj25hhIrzo39ObVNCgs4+uxLBVxmORcDis=",
-        "battlecn":"TQVefQF00lHYsnDFJD+tEkBI9dHI4X2dlE9TgtepYnc=",
-        "csdn":"3pGNOhaH3CAv2cFoFva/NkBI9dHI4X2dlE9TgtepYnc=",
-        "luexiao":"5fHvJfeWPc+g0JS+unEq0Ey/ls5ycdiNWdwrb7nYEpg=",
-        "linkedin":"5fHvJfeWPc+g0JS+unEq0Ey/ls5ycdiNWdwrb7nYEpg=",
-        "baidu":"aacZEknf9G7kKGBbolNjaEBI9dHI4X2dlE9TgtepYnc=",
-        "sanbeidanchi":"5fHvJfeWPc+g0JS+unEq0Ey/ls5ycdiNWdwrb7nYEpg=",
-        "yihaodian":"5fHvJfeWPc+g0JS+unEq0EJbFZJYumZ63yUtAX5v1VU=",
-        "shuishouji":"5fHvJfeWPc+g0JS+unEq0Ey/ls5ycdiNWdwrb7nYEpg=",
-        "evernote":"5fHvJfeWPc+g0JS+unEq0HBcQPu8D1F2e2fMRVJwoU8=",
-        "51job":"WtnuafI18sVeKwQYnehzAEBI9dHI4X2dlE9TgtepYnc=",
-        "windnle":"5fHvJfeWPc+g0JS+unEq0Ey/ls5ycdiNWdwrb7nYEpg=",
-        "8europ":"aacZEknf9G7kKGBbolNjaEBI9dHI4X2dlE9TgtepYnc=",
-        "dingxiangyuan":"AmjQfq6AwJsWEOWDWsrP30BI9dHI4X2dlE9TgtepYnc=",
-        "topcoder":"D3oWBKoUJUzFadk7MQsUzPPGzu2o3Mwqa1u6m+PMiqw=",
-        "pbccrc":"YuKyl1P8pAPqkWQ6JohLQkBI9dHI4X2dlE9TgtepYnc=",
-        "github":"UFYPpswX6ehwiS3r41WzqEBI9dHI4X2dlE9TgtepYnc=",
-        "slideshare":"VawSGufaDgVg1H8k1ZV7zkBI9dHI4X2dlE9TgtepYnc=",
-        "gmail old":"KVn59Plvd9MoQ8hjCJyVu4HKMQ6A89FTj48jT/6QrOo=",
-        "chinahr":"O8dL1iqBec29XYsYhykB+EBI9dHI4X2dlE9TgtepYnc=",
-        "liepin":"pP9sLMbF9xQF+jmM4TgmwkBI9dHI4X2dlE9TgtepYnc=",
-        "stackoverflow":"Ud7eGPY39HL8oqXWDG44NVxio7D2o7US6VLh7y6HfHo=",
-        "slideshare":"QYfI5X9qpFe9JcW9ciCXR2GkRSW/3ZbGAuk0c1gNYvM=",
-        "suishouji":"SEGuMENUQ0aTsSsG2plVKEBI9dHI4X2dlE9TgtepYnc=",
-        "POJ":"aacZEknf9G7kKGBbolNjaEBI9dHI4X2dlE9TgtepYnc=",
-        "leetcode":"NlsrKYu/W13jOFHk6OUAKkBI9dHI4X2dlE9TgtepYnc=",
-        "7yes":"fVLRjakX8NUNYY1X6IrnR/68Q7lfGfEqGcBjH73NVo0=",
-        "siemens":"hTed5CRoKJkm2RbTwTp9bkBI9dHI4X2dlE9TgtepYnc=",
-        "sf-express":"m9sW2voD80GV3hhPbDHp80BI9dHI4X2dlE9TgtepYnc=",
-        "live":"jlls3y89Vpc44E8aPaWVlEqgiNM37TlN93nLnYJGw53QeNK68tGEV707TsOsfpH5QlsVkli6ZnrfJS0Bfm/VVQ=="
-        }
+# seprate data from the code - DONE
+# auto insert new item to the data file
+# provide delete and modify the item by the key word
+
+box = {}
 
 if len(argv) == 2:
     script_name, account = argv
 else:
     account = raw_input("Input the web name:")
+
+# load all data to box
+with open("data") as f:
+    for line in f:
+        (key, val) = line.split(':')
+        box[key] = val
 
 candidate = []
 if account == 'all':
