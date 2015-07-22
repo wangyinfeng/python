8f0eb272d226418718ad7de8bd9162637029bd3c
wangyinfeng <wang.batman@gmail.com>
2015-07-22 16:58:14 +0800

    todo

---
+++ weibo_post.py
@@ -0,0 +1,9 @@
+#!/bin/python
+# Post weibo from command line, such as:
+# ~python weibo_post.py "the world you want to say"
+# step1 - post text 
+# step2 - post text and set the visibility, privite or public or share with friends
+# step3 - ...
+#
+# reference http://www.cnblogs.com/GentlemanMod/p/3263531.html
+
