b3a8eb8fe2525efb42e032af622bc3ad4726e98f
wangyinfeng <wang.batman@gmail.com>
2015-07-22 16:58:59 +0800

    another feature

--- weibo_post.py
+++ weibo_post.py
@@ -3,7 +3,8 @@
 # ~python weibo_post.py "the world you want to say"
 # step1 - post text 
 # step2 - post text and set the visibility, privite or public or share with friends
-# step3 - ...
+# step3 - support list and delete instance
+# step4 - ...
 #
 # reference http://www.cnblogs.com/GentlemanMod/p/3263531.html
 
