ac0c1800c6470288c9f88877714b25cca15b4a0f
wangyinfeng <wang.batman@gmail.com>
2015-07-24 13:39:11 +0800

    update

--- another_weibo.py
+++ another_weibo.py
@@ -3,6 +3,7 @@
 # enough for me.
 # my request is just post/delete/list/set the visibility,
 # that's all. 
+import os
 
 api_key='1804308942'
 status='hello'
@@ -13,4 +14,5 @@ delete_url='https://api.weibo.com/2/statuses/destroy.json'
 read_url=''
 visible=1
 
-curl -u batman.wang@qq.com:168802 -d "source=&status="hello"&visible=1" https://api.weibo.com/2/statuses/update.json
+curl -u batman.wang@qq.com:168802 -d source=&status="hello"&visible=1 https://api.weibo.com/2/statuses/update.json
+
