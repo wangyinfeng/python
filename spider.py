17951f82b420e533a4be2e766481e7389a3fa7a9
wangyinfeng <wang.batman@gmail.com>
2015-08-13 20:05:40 +0800

    update

--- spider.py
+++ spider.py
@@ -14,7 +14,7 @@ from bs4 import BeautifulSoup #parse a document to a tree representation
 import pdb
 
 #Error: maximum recursion depth exceeded while calling a Python object
-sys.setrecursionlimit(1000)
+sys.setrecursionlimit(100)
 #where do I use the recursion?
 
 file_name = ''
@@ -28,22 +28,19 @@ item_photo = "photos"
 
 
 def get_note_links():
-    global file_content
     global note_links
 
     url = base_url + people + '/' + item_note
-    source_code = requests.get(url)
+    web_source= requests.get(url)
     # just get the code, no headers or anything
-    plain_text = source_code.text
+    plain_text = web_source.text
     # BeautifulSoup objects can be sorted through easy
     soup = BeautifulSoup(plain_text)
 
-    title_divide = '\n' + '--' * 30 + '\n' + '--' * 30 + '\n'
     count = 1
     pages_info = soup.find('div', {'class':'paginator'})
     pages_num = int(pages_info.find('span', {'class':'thispage'}).get('data-total-page'))
     page_url = []
-#    page_url.append(url)
     for page in pages_info.find_all('a'):
         page_url.append(page.get('href'))
     #last one item is duplicated    
@@ -59,8 +56,8 @@ def get_note_links():
     #get the remain pages note links
     for next_page in page_url:
         url = next_page
-        source_code = requests.get(url)
-        plain_text = source_code.text
+        web_source = requests.get(url)
+        plain_text = web_source.text
         soup = BeautifulSoup(plain_text)
         list_soup = soup.findAll('div', {'class': 'note-header-container'})
         for notes in list_soup:
@@ -69,8 +66,9 @@ def get_note_links():
             count += 1
 
 def get_note_content(link):
-    source_code = requests.get(link)
-    plain_text = source_code.text
+    global file_content
+    web_source = requests.get(link)
+    plain_text = web_source.text
     soup = BeautifulSoup(plain_text)
     content = soup.find('div', {'id': 'link-report'})
 
@@ -84,10 +82,9 @@ def get_note_content(link):
     f.close()
     print file_content
 
-
 def do_spider():
     get_note_links()
-    print note_links
+#    print note_links
 
     if not os.path.exists(people):
         os.makedirs(people)
